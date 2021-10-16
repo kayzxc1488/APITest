d3a48502cf4462b2813611544000f6bd82b20828
kayzxc1488 <lilfenix1488@yandex.ru>
2021-10-16 17:48:11 +0300

    3124
    235

---
+++ RUBICtest/RUBICtest/Controllers/BudgetContext.cs
@@ -0,0 +1,19 @@
+﻿using RUBICtest.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RUBICtest.Controllers
+{
+    public class BudgetContext : DbContext
+    {
+        public BudgetContext(DbContextOptions<BudgetContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Budget> Budgets { get; set; }
+    }
+}
